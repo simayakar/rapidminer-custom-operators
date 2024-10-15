f13c295acc34b9ba83b84f4b203c44ae95eb0705
Simay Akar (마이) <64602775+simayakar@users.noreply.github.com>
2024-10-15 02:38:27 +0000

    Add

--- pages/Custom Operators.py
+++ pages/Custom Operators.py
@@ -1,3 +1,118 @@
 import streamlit as st
 
-st.write("Simos naber")
+st.header("Custom Operators")
+if st.button("한국어"):
+    st.subheader("1. 데이터 클리닝 (영어)")
+    st.write("**작업**")
+    st.write("- 사용자가 인터페이스에서 선택한 데이터 클리닝 기능을 기반으로, 지정된 텍스트 데이터 열에 대한 클리닝 작업을 자동으로 수행함")
+    st.write("**오퍼레이터**")
+    st.write("- Python Transformer")
+    st.write("**Input Type**")
+    st.write("- 데이터셋 텍스트 데이터 열")
+    st.write("**Ouput Type**")
+    st.write("- 데이터셋 + ’cleaned’ 열 추가")
+    
+    with st.popover("Show the working flow of the operator"):
+        st.image('images/dc_eng_flow.png')
+
+    st.divider()
+
+    st.subheader("2. 데이터 클리닝 (한국어)")
+    st.write("**작업**")
+    st.write("- 사용자가 인터페이스에서 선택한 데이터 클리닝 기능을 기반으로, 지정된 텍스트 데이터 열에 대한 클리닝 작업을 자동으로 수행함")
+    st.write("**오퍼레이터**")
+    st.write("- Python Transformer")
+    st.write("**Input Type**")
+    st.write("- 데이터셋 텍스트 데이터 열")
+    st.write("**Ouput Type**")
+    st.write("- 데이터셋 + ’cleaned’ 열 추가")
+    
+    with st.popover("Show the working flow of the operator"):
+        st.image('images/dc_kor_flow.png')
+
+    st.divider()
+
+    st.subheader("3. KoNLPy Tokenization")
+
+
+
+
+
+
+
+else:
+    st.subheader("1. Data Cleaning (English)")
+    st.write("**Work**")
+    st.write("- Automatically perform cleaning operations on the specified text data column based on the data cleaning functions selected by the user in the interface.")
+    st.write("**Operator Used**")
+    st.write("- Python Transformer")
+    st.write("**Input Type**")
+    st.write("- DataFrame Text Column")
+    st.write("**Ouput Type**")
+    st.write("- DataFrame Updated with a column 'cleaned'")
+
+    with st.popover("Show the working flow of the operator"):
+        st.image('images/dc_eng_flow.png')
+
+    st.divider()
+
+    st.subheader("2. Data Cleaning (Korean)")
+    st.write("**Work**")
+    st.write("- Automatically perform cleaning operations on the specified text data column based on the data cleaning functions selected by the user in the interface.")
+    st.write("**Operator Used**")
+    st.write("- Python Transformer")
+    st.write("**Input Type**")
+    st.write("- DataFrame Text Column")
+    st.write("**Ouput Type**")
+    st.write("- DataFrame Updated with a column 'cleaned'")
+
+    with st.popover("Show the working flow of the operator"):
+        st.image('images/dc_kor_flow.png')
+
+    st.divider()
+    
+    st.subheader("3. KoNLPy Tokenization")
+    st.markdown("- Komoran Tokenizer")
+    st.markdown("- Hannanum Tokenizer")
+    st.markdown("- Kkma Tokenizer")
+    st.markdown("- Okt Tokenizer")
+
+    option = st.selectbox(
+     'Select a tokenizer',
+     ('Komoran Tokenizer', 'Hannanum Tokenizer', 'Kkma Tokenizer', 'Okt Tokenizer'))
+    
+    if option == 'Komoran Tokenizer':
+        st.write("***Functions***")
+        st.markdown("- morphs \n- pos \n- nouns")
+    elif option == 'Hannanum Tokenizer':
+        st.write("***Functions***")
+        st.markdown("- morphs \n- pos \n- nouns \n- analyze")
+    elif option == 'Kkma Tokenizer':
+        st.write("***Functions***")
+        st.markdown("- morphs \n- pos \n- nouns \n- sentences")
+    elif option == 'Okt Tokenizer':
+        st.write("***Functions***")
+        st.markdown("- morphs \n- pos \n- nouns \n- phrases \n- normalize")
+
+
+    @st.container("siimmmm")
+    def vote(item):
+        st.write(f"{item}")
+        reason = st.text_input("Because...")
+        if st.button("Submit"):
+            st.session_state.vote = {"item": item, "reason": reason}
+            st.rerun()
+
+    if "vote" not in st.session_state:
+        st.write("Select a tokenizer")
+        if st.button("Komoran Tokenizer"):
+            vote("Komoran Tokenizer")
+        if st.button("B"):
+            vote("B")
+    else:
+        f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
+
+
+
+
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,38 +3,17 @@ import pandas as pd
 
 import streamlit_antd_components as sac
 
-sac.menu([
-    sac.MenuItem('home', icon='house-fill', tag=[sac.Tag('Tag1', color='green'), sac.Tag('Tag2', 'red')]),
-    sac.MenuItem('products', icon='box-fill', children=[
-        sac.MenuItem('apple', icon='apple'),
-        sac.MenuItem('other', icon='git', description='other items', children=[
-            sac.MenuItem('google', icon='google', description='item description'),
-            sac.MenuItem('gitlab', icon='gitlab'),
-            sac.MenuItem('wechat', icon='wechat'),
-        ]),
-    ]),
-    sac.MenuItem('disabled', disabled=True),
-    sac.MenuItem(type='divider'),
-    sac.MenuItem('link', type='group', children=[
-        sac.MenuItem('antd-menu', icon='heart-fill', href='https://ant.design/components/menu#menu'),
-        sac.MenuItem('bootstrap-icon', icon='bootstrap-fill', href='https://icons.getbootstrap.com/'),
-    ]),
-], index=2, size='sm', color='red', open_all=True)
 
 st.title("RapidMiner Custom Operators :gear:")
 
 st.divider()
 
-st.header("Python Operators")
+st.header("Rapidminer Python Operators")
 
-df = pd.DataFrame(
-    {
-     'Operator Name': ["Python Transform", 
-     "Python Execute", "Python Learner", "Python Forecast"]}
-)
-
-st.write(df)
-
+st.markdown("- Python Transform")
+st.markdown("- Python Execute")
+st.markdown("- Python Learner")
+st.markdown("- Python Forecast")
 
 st.header('Operator Info')
 
@@ -43,10 +22,29 @@ option = st.selectbox(
      ('Python Execute', 'Python Transform', 'Python Forecast', 'Python Learner'))
 
 if option == 'Python Execute':
-    st.write("This operator enables users to run Python scripts within a RapidMiner process. It allows for the seamless integration of Python functions and libraries into RapidMiner workflows, passing input data from RapidMiner to Python and returning processed results back to RapidMiner.")
+    if st.button("한국어"):
+        st.markdown("- Python 코드를 실행하여 데이터 전처리, 분석 등을 수행")
+        st.markdown("- Pandas, NumPy 등 라이브러리를 활용 가능")
+        st.markdown("- Python에서 처리한 데이터를 RapidMiner로 반환")
+    else:
+        st.write("- This operator enables users to run Python scripts within a RapidMiner process. It allows for the seamless integration of Python functions and libraries into RapidMiner workflows, passing input data from RapidMiner to Python and returning processed results back to RapidMiner.")
 elif option == 'Python Transform':
-    st.write("This operator is used to transform data using a custom Python script. It is particularly useful for performing complex transformations or feature engineering that may not be available in RapidMiner's native toolset, while still keeping the process within the RapidMiner environment.")
+    if st.button("한국어"):
+        st.markdown("- Python 코드를 사용해 데이터를 변환")
+        st.markdown("- 텍스트 벡터화 및 사용자 정의 변환 작업에 적합")
+        st.markdown("- Python 기반 모델을 RapidMiner 프로세스에서 사용할 수 있음")
+    else:
+        st.write("- This operator is used to transform data using a custom Python script. It is particularly useful for performing complex transformations or feature engineering that may not be available in RapidMiner's native toolset, while still keeping the process within the RapidMiner environment.")
 elif option == 'Python Learner':
-    st.write("The Python Learner operator lets you create machine learning models in Python, using popular libraries such as scikit-learn. Once the model is trained, it can be applied within RapidMiner for predictions, evaluations, and performance assessments, combining Python’s flexibility with RapidMiner's data handling capabilities.")
+    if st.button("한국어"):
+        st.markdown("- 머신러닝 알고리즘으로 데이터 학습 및 모델 생성")
+        st.markdown("- Python Transformer를 통해 사용자 정의 모델 학습 가능")
+    else:
+        st.write("- The Python Learner operator lets you create machine learning models in Python, using popular libraries such as scikit-learn. Once the model is trained, it can be applied within RapidMiner for predictions, evaluations, and performance assessments, combining Python’s flexibility with RapidMiner's data handling capabilities.")
 elif option == 'Python Forecast':
-    st.write("This operator is designed for time series forecasting using Python libraries. It allows users to build and apply forecasting models in Python and integrate the results with other RapidMiner processes for further analysis or reporting.")
+    if st.button("한국어"):
+        st.markdown("- 시계열 데이터를 예측")
+        st.markdown("- Python에서 예측 모델을 학습해 RapidMiner에서 사용 가능")
+    else:
+        st.write("- This operator is designed for time series forecasting using Python libraries. It allows users to build and apply forecasting models in Python and integrate the results with other RapidMiner processes for further analysis or reporting.")
+
