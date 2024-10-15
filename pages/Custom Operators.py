a824165dc287374cfadb009ea1e3c1f681c4fe6b
Simay Akar (마이) <64602775+simayakar@users.noreply.github.com>
2024-10-15 05:38:49 +0000

    Updating operator pages

--- pages/Custom Operators.py
+++ pages/Custom Operators.py
@@ -12,7 +12,7 @@ if st.button("한국어"):
     st.write("**Ouput Type**")
     st.write("- 데이터셋 + ’cleaned’ 열 추가")
     
-    with st.popover("Show the working flow of the operator"):
+    with st.popover("Show the workflow of the operator"):
         st.image('images/dc_eng_flow.png')
 
     st.divider()
@@ -27,16 +27,67 @@ if st.button("한국어"):
     st.write("**Ouput Type**")
     st.write("- 데이터셋 + ’cleaned’ 열 추가")
     
-    with st.popover("Show the working flow of the operator"):
+    with st.popover("Show the workflow of the operator"):
         st.image('images/dc_kor_flow.png')
 
     st.divider()
+    col1, col2 = st.columns([3, 1])
+    col1.subheader("3. KoNLPy 토큰화")
+    col1.write("**작업**")
+    col1.write("- 한국어 텍스트 데이터 토큰화")
+    col1.write("**오퍼레이터**")
+    col1.write("- Python Transformer")
+    col1.write("**Input Type**")
+    col1.write("- 데이터셋 텍스트 데이터 열")
+    col1.write("**Ouput Type**")
+    col1.write("- 데이터셋 + ’tokens’ 열 추가")
+    col2.write("")
+    col2.write("**Tokenizers**")
+    col2.markdown("- Komoran Tokenizer")
+    col2.markdown("- Hannanum Tokenizer")
+    col2.markdown("- Kkma Tokenizer")
+    col2.markdown("- Okt Tokenizer")
 
-    st.subheader("3. KoNLPy Tokenization")
 
-
-
-
+    option = st.selectbox(
+     'Select a tokenizer',
+     ( 'Select a tokenizer', 'Komoran Tokenizer', 'Hannanum Tokenizer', 'Kkma Tokenizer', 'Okt Tokenizer'))
+    
+   
+    c = st.container(border=True)
+    if option == 'Select a tokenizer':
+        st.write("")
+    elif option == 'Komoran Tokenizer':
+        c.write("***Functions***")
+        c.markdown("- morphs \n- pos \n- nouns")
+        c.write("**Note:**")
+        c.markdown("- 특정 이름이나 구문이 단일 토큰으로 처리되도록 하는 'userdict'라는 매개변수를 지원")
+        c.markdown("- 사용자는 자신의 `.txt` 파일을 업로드하여 맞춤형 토큰화를 설정할 수 있음")
+        c.image('images/komoran_flow.png')
+    elif option == 'Hannanum Tokenizer':
+        c.write("***Functions***")
+        c.markdown("- morphs \n- pos \n- nouns \n- analyze")
+        c.image('images/hannanum_flow.png')
+    elif option == 'Kkma Tokenizer':
+        c.write("***Functions***")
+        c.markdown("- morphs \n- pos \n- nouns \n- sentences")
+        c.image('images/kkma_flow.png')
+    elif option == 'Okt Tokenizer':
+        c.write("***Functions***")
+        c.markdown("- morphs \n- pos \n- nouns \n- phrases \n- normalize")
+        c.image('images/okt_flow.png')
+    
+    st.subheader("4. 텍스트 전처리")
+    st.write("**작업**")
+    st.write("- 텍스트 데이터를 사용하여 다양한 벡터화 방식을 적용하는 사용자 정의 파이썬 변환기")
+    st.write("- 토큰화된 텍스트 컬럼을 선택하여 TF-IDF, Term Frequency, Term Occurences, 또는 Binary 벡터화를 수행할 수 있음")
+    st.write("**오퍼레이터**")
+    st.write("- Python Transformer")
+    st.write("**Input Type**")
+    st.write("- 데이터셋 토큰리스트 열")
+    st.write("**Ouput Type**")
+    st.write("1. 데이터셋")
+    st.write("2. 벡터화 결과 데이터셋")
 
 
 
@@ -47,11 +98,11 @@ else:
     st.write("**Operator Used**")
     st.write("- Python Transformer")
     st.write("**Input Type**")
-    st.write("- DataFrame Text Column")
+    st.write("- Dataset Text Column")
     st.write("**Ouput Type**")
-    st.write("- DataFrame Updated with a column 'cleaned'")
+    st.write("- Dataset updated with a column 'cleaned'")
 
-    with st.popover("Show the working flow of the operator"):
+    with st.popover("Show the workflow of the operator"):
         st.image('images/dc_eng_flow.png')
 
     st.divider()
@@ -62,55 +113,77 @@ else:
     st.write("**Operator Used**")
     st.write("- Python Transformer")
     st.write("**Input Type**")
-    st.write("- DataFrame Text Column")
+    st.write("- Dataset Text Column")
     st.write("**Ouput Type**")
-    st.write("- DataFrame Updated with a column 'cleaned'")
+    st.write("- Dataset updated with a column 'cleaned'")
 
-    with st.popover("Show the working flow of the operator"):
+    with st.popover("Show the workflow of the operator"):
         st.image('images/dc_kor_flow.png')
 
     st.divider()
+
+    col1, col2 = st.columns([3, 1])
+    col1.subheader("3. KoNLPy Tokenization")
+    col1.write("**Work**")
+    col1.write("- Korean text tokenization")
+    col1.write("**Operator Used**")
+    col1.write("- Python Transformer")
+    col1.write("**Input Type**")
+    col1.write("- Dataset Text Column")
+    col1.write("**Ouput Type**")
+    col1.write("- Dataset updated with a column 'tokens'")
     
-    st.subheader("3. KoNLPy Tokenization")
-    st.markdown("- Komoran Tokenizer")
-    st.markdown("- Hannanum Tokenizer")
-    st.markdown("- Kkma Tokenizer")
-    st.markdown("- Okt Tokenizer")
+    col2.write("")
+    col2.write("**Tokenizers**")
+    col2.markdown("- Komoran Tokenizer")
+    col2.markdown("- Hannanum Tokenizer")
+    col2.markdown("- Kkma Tokenizer")
+    col2.markdown("- Okt Tokenizer")
+
 
     option = st.selectbox(
      'Select a tokenizer',
-     ('Komoran Tokenizer', 'Hannanum Tokenizer', 'Kkma Tokenizer', 'Okt Tokenizer'))
+     ( 'Select a tokenizer', 'Komoran Tokenizer', 'Hannanum Tokenizer', 'Kkma Tokenizer', 'Okt Tokenizer'))
     
-    if option == 'Komoran Tokenizer':
-        st.write("***Functions***")
-        st.markdown("- morphs \n- pos \n- nouns")
+   
+    c = st.container(border=True)
+    if option == 'Select a tokenizer':
+        st.write("")
+    elif option == 'Komoran Tokenizer':
+        c.write("***Functions***")
+        c.markdown("- morphs \n- pos \n- nouns")
+        c.write("**Note:**")
+        c.markdown("- You can specify the 'userdict' parameter to ensure that specific names or phrases are treated as single tokens.")
+        c.markdown("- Users can upload their own `.txt` file to configure custom tokenization.")
+        c.image('images/komoran_flow.png')
     elif option == 'Hannanum Tokenizer':
-        st.write("***Functions***")
-        st.markdown("- morphs \n- pos \n- nouns \n- analyze")
+        c.write("***Functions***")
+        c.markdown("- morphs \n- pos \n- nouns \n- analyze")
+        c.image('images/hannanum_flow.png')
     elif option == 'Kkma Tokenizer':
-        st.write("***Functions***")
-        st.markdown("- morphs \n- pos \n- nouns \n- sentences")
+        c.write("***Functions***")
+        c.markdown("- morphs \n- pos \n- nouns \n- sentences")
+        c.image('images/kkma_flow.png')
     elif option == 'Okt Tokenizer':
-        st.write("***Functions***")
-        st.markdown("- morphs \n- pos \n- nouns \n- phrases \n- normalize")
+        c.write("***Functions***")
+        c.markdown("- morphs \n- pos \n- nouns \n- phrases \n- normalize")
+        c.image('images/okt_flow.png')
+   
+    st.subheader("4. Text Processing")
+    st.write("**Work**")
+    st.write("- A custom Python transformer that applies various vectorization methods to text data.")
+    st.write("- You can select a tokenized text column to perform TF-IDF, Term Frequency, Term Occurrences, or Binary vectorization.")
+    st.write("**Operator Used**")
+    st.write("- Python Transformer")
+    st.write("**Input Type**")
+    st.write("- Dataset token list column")
+    st.write("**Ouput Type**")
+    st.write("1. Dataset")
+    st.write("2. Vectorized dataset")
+    with st.popover("Show the workflow of the operator"):
+        st.image('images/processing_flow.png')
 
 
-    @st.container("siimmmm")
-    def vote(item):
-        st.write(f"{item}")
-        reason = st.text_input("Because...")
-        if st.button("Submit"):
-            st.session_state.vote = {"item": item, "reason": reason}
-            st.rerun()
-
-    if "vote" not in st.session_state:
-        st.write("Select a tokenizer")
-        if st.button("Komoran Tokenizer"):
-            vote("Komoran Tokenizer")
-        if st.button("B"):
-            vote("B")
-    else:
-        f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
 
 
 
